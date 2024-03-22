cc4d125cfd2b5c100aa58699fab3fc01974bba77
vamsii <korlavamsi2003@gmail.com>
2024-03-22 19:40:56 +0530

    add baners

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='  mx-20 mt-2 flex flex-row'>
+    <div className='  mx-20 mt-2 flex flex-row justify-center' >
       <img src='Banners/temple_it.png 'alt='Banner' class='rounded-xl'/>
    
     </div>
