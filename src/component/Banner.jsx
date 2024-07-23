6366f802673cb057b76124d4f4aee0ed3b455f1f
Arjun Rao <59176699+Arjun-Rao-student@users.noreply.github.com>
2024-07-23 07:42:43 +0530

    Update Banner.jsx

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Banner() {
   return (
     <div className='mx-4 md:mx-20 mt-2 flex flex-row justify-center'>
-      <img src='Banners/temple_it.png' alt='Banner' className='rounded-xl max-w-full h-auto' />
+      <img src='Banners/fresh&hy.png' alt='Banner' className='rounded-xl max-w-full h-auto' />
     </div>
   );
 }
